b0fab935b8ce1ad4f0d813febe7e83f829543980
Damian Mielczarek <mielczarek.damian99@o2.pl>
2024-01-26 23:03:32 +0100

    Created dummy types

--- src/main/kotlin/put/poznan/development/DummyDataLoader.kt
+++ src/main/kotlin/put/poznan/development/DummyDataLoader.kt
@@ -5,6 +5,8 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.core.annotation.Order
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
+import put.poznan.section.type.Type
+import put.poznan.section.type.TypeRepository
 import put.poznan.user.UserCMS
 import put.poznan.user.UserCMSRepository
 import put.poznan.user.role.Role
@@ -16,6 +18,7 @@ import put.poznan.user.role.RoleRepository
 class DummyDataLoader(
     private val roleRepository: RoleRepository,
     private val userCMSRepository: UserCMSRepository,
+    private val typeRepository: TypeRepository,
     private val encoder: PasswordEncoder
 ): CommandLineRunner {
     override fun run(vararg args: String?) {
@@ -38,9 +41,29 @@ class DummyDataLoader(
                 adm.user = wojtek
 
                 roleRepository.saveAll(listOf(mod, adm))
+                val type1 = Type(type = "BestOffers", hidden = false)
+                val type2 = Type(type = "Categories", hidden = false)
+                val type3 = Type(type = "Contact", hidden = false)
+                val type4 = Type(type = "Infoboxes", hidden = false)
+                val type5 = Type(type = "MainSlider", hidden = false)
+                val type6 = Type(type = "OurTeam", hidden = false)
+                val type7 = Type(type = "PopularProperties", hidden = false)
+                val type8 = Type(type = "Testimonials", hidden = false)
+                val type9 = Type(type = "WhoWeAre", hidden = false)
+                val type10 = Type(type = "WhyUs", hidden = false)
+
+                type1.user = wojtekIn
+                type2.user = wojtekIn
+                type3.user = wojtekIn
+                type4.user = wojtekIn
+                type5.user = wojtekIn
+                type6.user = wojtekIn
+                type7.user = wojtekIn
+                type8.user = wojtekIn
+                type9.user = wojtekIn
+                type10.user = wojtekIn
+                typeRepository.saveAll(listOf(type1, type2, type3, type4, type5, type6, type7, type8, type9, type10))
             }
-
-
 
         } catch (exception: Exception){
             throw Exception(exception.message)
